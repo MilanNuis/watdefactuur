78a777282f07afdb6b6f218ca1a62c5290a9cd43
Milan Nuis <milannuis@gmail.com>
2026-02-11 09:56:24 +0100

    removed debug

--- app/Http/Controllers/MollieController.php
+++ app/Http/Controllers/MollieController.php
@@ -21,7 +21,6 @@ class MollieController extends Controller
             }
 
             $payment = Mollie::api()->payments->get($paymentId);
-            Log::info('Payment', ['payment' => $payment]);
 
             $userId = null;
             if (isset($payment->metadata) && isset($payment->metadata->user_id)) {
