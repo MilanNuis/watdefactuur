d2503305c82770a6507f9dcf6178e9d95ede5f1d
Milan Nuis <milannuis@gmail.com>
2026-02-11 09:10:03 +0100

    fix build error

--- resources/js/Components/ui/tabs.tsx
+++ resources/js/Components/ui/tabs.tsx
@@ -21,7 +21,7 @@ const TabsList = React.forwardRef<
 TabsList.displayName = TabsPrimitive.List.displayName;
 
 const TabsTrigger = React.forwardRef<
-    React.ElementRef<typeof TabsPrimitivewe.Trigger>,
+    React.ElementRef<typeof TabsPrimitive.Trigger>,
     React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
 >(({ className, ...props }, ref) => (
     <TabsPrimitive.Trigger
